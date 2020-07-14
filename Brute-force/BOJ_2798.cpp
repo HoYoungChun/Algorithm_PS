d7abe13ac21e65d35791693de331573135f1f5ba
Hoyoung Chun <ho723@naver.com>
2020-07-14 17:05:19 +0900

    Create BOJ_2798.cpp

---
+++ Brute-force/BOJ_2798.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main(void) 
+{
+	int arr[101];
+	int res = 0;
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < N - 2; i++) {
+		for (int j = i + 1; j < N - 1; j++) {
+			for (int k = j + 1; k < N ; k++) {
+				int sum = arr[i] + arr[j] + arr[k];
+				if (sum <= M && sum > res)
+					res = sum;
+			}
+		}
+	}
+
+	cout << res;
+
+	return 0;
+}
