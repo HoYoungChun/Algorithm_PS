11329b59a45fac2ec60d7f707e6032e5d835c110
HoYoungChun <ho723@naver.com>
2020-10-17 22:08:16 +0900

    Create BOJ_2751.cpp

---
+++ Sorting/BOJ_2751.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+ 
+using namespace std;
+int arr[1000001];
+ 
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    int testnum = 0;
+    cin >> testnum;
+    for (int i = 0; i < testnum; i++) {
+        cin >> arr[i];
+    }
+ 
+    sort(arr, arr + testnum);
+ 
+    for (int i = 0; i < testnum; i++) {
+        cout << arr[i]<<"\n";
+    }
+}
