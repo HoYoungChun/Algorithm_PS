c18bcbad3f3d44cf0901d94ff377689184a359fe
HoYoungChun <ho723@naver.com>
2021-07-27 23:23:35 +0900

    Update LC_344.py

--- String/LC_344.py
+++ String/LC_344.py
@@ -1,6 +1,23 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        #s.reverse()로 된다
-        #s=s[::-1] #공간복잡도 O(1)로 제한되어있어서 안됨 s[:]=s[::-1]은 된다
+    	s[:]=s[::-1]
+        
+        
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+
+        
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s)-1
+        while left < right:
+          s[left], s[right] = s[right], s[left]
+          left += 1
+          right -= 1
+
+        
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
         for i in range(0,len(s)//2):
             s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i] #교환
