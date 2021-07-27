327ad72ad458a30955ed42d0f7155c2487c54a03
HoYoungChun <ho723@naver.com>
2021-07-27 21:13:30 +0900

    Update LC_125.py

--- String/LC_125.py
+++ String/LC_125.py
@@ -3,7 +3,8 @@ class Solution:
         s = s.lower().replace(' ','') #모두 소문자로+공백제거
         refined_s = '' #숫자,문자만 남길 문자열
         for one_s in s:
-          if one_s.isdigit() or one_s.isalpha(): #숫자거나 문자일때만
+          #if one_s.isdigit() or one_s.isalpha(): #숫자거나 문자일때만
+          if one_s.isalnum(): #숫자거나 문자일때만
             refined_s+= one_s
         
         if refined_s == refined_s[::-1]: #뒤집어도 같다면
