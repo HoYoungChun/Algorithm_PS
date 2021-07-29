7ba6527e6516676f2f336273d3eea21b83cc4d13
HoYoungChun <ho723@naver.com>
2021-07-29 15:39:56 +0900

    Update LC_49.py

--- String/LC_49.py
+++ String/LC_49.py
@@ -5,3 +5,11 @@ class Solution:
           key = tuple(sorted(str)) # 내부 알파벳 정렬해서 key값으로
           new_dict[key].append(str)
         return list(new_dict.values())
+
+    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        new_dict=defaultdict(list) #기본 value가 []
+        for str in strs:
+          new_dict[''.join(sorted(str))].append(str)
+        return list(new_dict.values())
