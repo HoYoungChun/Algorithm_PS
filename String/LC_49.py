c0dbc771f7c39c0674d6b19c7939d855113b176a
HoYoungChun <ho723@naver.com>
2021-07-29 15:19:25 +0900

    Create LC_49.py

---
+++ String/LC_49.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        new_dict=defaultdict(list) #기본 value가 []
+        for str in strs:
+          key = tuple(sorted(str)) # 내부 알파벳 정렬해서 key값으로
+          new_dict[key].append(str)
+        return list(new_dict.values())
