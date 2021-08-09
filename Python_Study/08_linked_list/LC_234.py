60a0f19eb193fcca8a43515544268ef47a167a0c
HoYoungChun <ho723@naver.com>
2021-08-09 15:25:54 +0900

    Update LC_234.py

--- Python_Study/08_linked_list/LC_234.py
+++ Python_Study/08_linked_list/LC_234.py
@@ -12,3 +12,40 @@ class Solution:
             head = head.next
             
         return num_list == num_list[::-1] #뒤집어서 같은지(O(N))
+
+    
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        q: Deque = collections.deque() #deque 선언
+        
+        if not head: #빈 연결리스트일때
+            return True
+        
+        node = head
+        while node != None:
+            q.append(node.val)
+            node = node.next
+        
+        while len(q)>1:
+            if q.popleft() != q.pop():
+                return False #하나라도 앞,뒤 대칭되는위치에 다른게 있으면 False
+            
+        return True
+    
+    
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        rev = None #역순 연결 리스트
+        slow=fast=head #시작할때는 모두 head에서
+        
+        #런너를 이용해 역순 연결 리스트 구성
+        while fast and fast.next:
+            fast = fast.next.next #두칸씩 이동
+            rev,rev.next,slow = slow,rev,slow.next
+        if fast:
+            slow=slow.next #한칸씩 이동
+        
+        #팰린드롬 여부 확인
+        while rev and rev.val == slow.val:
+            slow,rev = slow.next,rev.next
+        return not rev
